f346062dfbe45d4c859305fdb8136e6638e8cb8c
shatanyumi <1143005768@qq.com>
2020-06-22 14:58:00 +0800

    echo_mutilThread/

--- echo_mutilThread/tcp_echo_cli.c
+++ echo_mutilThread/tcp_echo_cli.c
@@ -8,148 +8,283 @@
 #include <errno.h>
 #include <syscall.h>
 #include <pthread.h>
+#include <signal.h>
 
-#define MAX_CMD_STR 100
+#define MAX_CMD_STR 124
+#define MAXLINE 8192
+#define bprintf(fp,format,...)\
+        if(fp==NULL){printf(format,##__VA_ARGS__);}\
+        else {printf(format,##__VA_ARGS__);\
+                fprintf(fp,format,##__VA_ARGS__);fflush(fp);}
 
 typedef struct sockaddr* pSA;
+int  pin;
+FILE *fp_res;
+pthread_mutex_t mutex;
+int TID;
+int clientfd;
 
 typedef struct PDU{
     int PIN;
     int LEN;
-    char BUF[MAX_CMD_STR+1];
+    char BUFFER[MAX_CMD_STR];
 }pdu;
 
-int pin;
-FILE *res;
-long tid;
-
-void *Write(void *arg){
-    pthread_detach(pthread_self());
-    int sockfd = *(int*)arg;
-    pdu echo_rqt_pdu;
-    char buf[MAX_CMD_STR+1];
-    char td_file[MAX_CMD_STR];
-
-    sprintf(td_file,"td%d.txt",pin);
-    FILE *td = fopen(td_file,"r");
-    if(td == NULL){
-        printf("td_file open failed!\n");
-        exit(1);
-    }
-    while (fgets(buf, sizeof(buf), td) != NULL) {
-
-        if (strncmp(buf, "exit", 4) == 0){
-            shutdown(sockfd,SHUT_WR);
-            memset(buf,0,sizeof(buf));
-            sprintf(buf,"[cli](%ld) shutdown is called with SHUT_WR!",tid);
-            fputs(buf,res);
-        }
-
-        int len = strnlen(buf, MAX_CMD_STR);
-        buf[len] = '\0';
-
-        echo_rqt_pdu.PIN = pin;
-        echo_rqt_pdu.LEN = len;
-        strcpy(echo_rqt_pdu.BUF, buf);
-
-        if(write(sockfd,&echo_rqt_pdu,sizeof(pdu)) == -1){
-            printf("write echo_rqt_pdu error \n");
-            exit(1);
-        }
-
-    }
-    
-    fclose(td);
-}
-
-int echo_rqt(int sockfd) {
-    char buf[MAX_CMD_STR+1];
-    pdu echo_rep_pdu,echo_rqt_pdu;
-    pthread_t socket_write;
-
-    if(pthread_create(&socket_write,NULL,Write,&sockfd) != 0){
-        printf("pthread_create failed!\n");
-        exit(1);
-    }
-
-        pthread_join(socket_write,NULL);
-        memset(buf,0,sizeof(buf));
-        sprintf(buf,"[cli](%ld) pthread_detach is done!",tid);
-        fputs(buf,res);
-
-
-        int rd;
-        while(1){
-            rd = read(sockfd,&echo_rep_pdu,sizeof(pdu));
-            if(rd == 0) break;
-            if(rd == -1){
-                printf("[cli] read echo_rep_pdu error \n");
-                exit(1);
-            }
-            char res_buf[MAX_CMD_STR+8];
-            sprintf(res_buf,"[echo_rep](%ld) %s",tid,echo_rep_pdu.BUF);
-            fputs(res_buf,res);
-        }
-
-    return 0;
-}
-
-int main(int argc,char *argv[])
+void unix_error(char *msg) /* Unix-style error */
 {
-    struct sockaddr_in srv_addr;
-    int connfd;
-    if(argc != 4){
-        printf("Usage:%s <IP> <PORT> <mutl_num>\n", argv[0]);
-        return 0;
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+
+void posix_error(int code, char *msg) /* Posix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(code));
+    exit(0);
+}
+
+void Close(int fd)
+{
+    if(close(fd)<0)
+        unix_error("Close error");
+}
+
+void Fclose(FILE *fp)
+{
+    if(fclose(fp)!=0)
+        unix_error("Fclose error");
+}
+
+int Socket(int domain, int type, int protocol)
+{
+    int rc;
+
+    if ((rc = socket(domain, type, protocol)) < 0)
+        unix_error("Socket error");
+    return rc;
+}
+
+void Connect(int sockfd, struct sockaddr *serv_addr, int addrlen)
+{
+    int rc;
+
+    if ((rc = connect(sockfd, serv_addr, addrlen)) < 0)
+        unix_error("Connect error");
+}
+
+FILE *Fopen(const char *filename,const char *mode)
+{
+    FILE *fp;
+    if((fp = fopen(filename,mode))==NULL)
+        unix_error("Fopen error");
+    return fp;
+}
+
+ssize_t rio_readn(int fd, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nread = read(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) /* Interrupted by sig handler return */
+                nread = 0;      /* and call read() again */
+            else
+                return -1;      /* errno set by read() */
+        }
+        else if (nread == 0)
+            break;              /* EOF */
+        nleft -= nread;
+        bufp += nread;
     }
+    return (n - nleft);         /* return >= 0 */
+}
+
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR)  /* Interrupted by sig handler return */
+                nwritten = 0;    /* and call write() again */
+            else
+                return -1;       /* errno set by write() */
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
+    }
+    return n;
+}
+
+
+void Rio_writen(int fd,void *userbuf,size_t n)
+{
+    if(rio_writen(fd,userbuf,n) != n)
+        unix_error("Rio_writen error");
+}
+
+void Rio_readn(int fd,void *userbuf,size_t n)
+{
+    if(rio_readn(fd,userbuf,n)<0)
+        unix_error("Rio_read error");
+
+}
+
+int open_clientfd(char *ip,char *port)
+{
+    int fd;
+    struct sockaddr_in srv_addr;
+
+    fd = Socket(AF_INET,SOCK_STREAM,0);
 
     bzero(&srv_addr,sizeof(srv_addr));
     srv_addr.sin_family = AF_INET;
-    srv_addr.sin_port = htons(atoi(argv[2]));
-    inet_pton(AF_INET,argv[1],&srv_addr.sin_addr);
-    pin = atoi(argv[3]);
+    srv_addr.sin_port = htons(atoi(port));
+    inet_pton(AF_INET,ip,&srv_addr.sin_addr);
 
-    tid = syscall(SYS_gettid);
-    char file_name[MAX_CMD_STR];
-    sprintf(file_name,"stu_cli_res_%d.txt",pin);
-    printf("[cli](%ld) %s is created!\n",tid,file_name);
-    FILE *res = fopen(file_name,"wb");
-    if(res == NULL){
-        printf("stu_cli_res_PIN.txt open failed!\n");
-        exit(1);
-    }
+    Connect(fd,(pSA)&srv_addr,sizeof(struct sockaddr_in));
 
-    connfd = socket(AF_INET,SOCK_STREAM,0);
-    if(connfd == -1){
-        printf("[cli] socket error\n");
-        exit(1);
-    }
-    char buf[MAX_CMD_STR+1];
-    do{
-            int result = connect(connfd,(pSA)&srv_addr,sizeof(srv_addr));
-            if(result == 0){
-                memset(buf,0,sizeof(buf));
-                sprintf(buf,"[cli](%ld) server[%s:%s] is connected!",tid,argv[1],argv[2]);
-                fputs(buf,res);
-                if (echo_rqt(connfd) == 0){
-                    break;
-                }
-            }
-            else if(result == -1 && errno == EINTR){
-                continue;
-            }
-        }while(1);
+    return fd;
+}
 
-    close(connfd);
-    sprintf(buf,"[cli](%ld) connfd is closed!",tid);
-    fputs(buf,res);
-
-    sprintf(buf,"[cli](%ld) parent thread is going to exit!",tid);
-    fputs(buf,res);
-    fclose(res);
-
-    printf("[cli](%ld) %s is closed!\n",tid,file_name);
-    return 0;
+int Open_clientfd(char *ip,char *port)
+{
+    int rc;
+    if((rc = open_clientfd(ip,port))<0)
+        unix_error("Open_clientfd Unix error");
+    return rc;
 }
 
 
+void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp,
+                    void * (*routine)(void *), void *argp)
+{
+    int rc;
+
+    if ((rc = pthread_create(tidp, attrp, routine, argp)) != 0)
+        posix_error(rc, "Pthread_create error");
+}
+
+void Pthread_cancel(pthread_t tid) {
+    int rc;
+
+    if ((rc = pthread_cancel(tid)) != 0)
+        posix_error(rc, "Pthread_cancel error");
+}
+
+void Pthread_join(pthread_t tid, void **thread_return) {
+    int rc;
+
+    if ((rc = pthread_join(tid, thread_return)) != 0)
+        posix_error(rc, "Pthread_join error");
+}
+
+void Pthread_detach(pthread_t tid) {
+    int rc;
+
+    if ((rc = pthread_detach(tid)) != 0)
+        posix_error(rc, "Pthread_detach error");
+}
+
+void Pthread_exit(void *retval) {
+    pthread_exit(retval);
+}
+
+pthread_t Pthread_self(void) {
+    return pthread_self();
+}
+
+void Pthread_once(pthread_once_t *once_control, void (*init_function)()) {
+    pthread_once(once_control, init_function);
+}
+
+void Read()
+{
+    int len,Pin;
+    pdu echo_rep_msg;
+
+    while(rio_readn(clientfd,&echo_rep_msg.PIN,sizeof(echo_rep_msg.PIN))>0){
+        Rio_readn(clientfd,&echo_rep_msg.LEN,sizeof(echo_rep_msg.LEN));
+        Pin = ntohl(echo_rep_msg.PIN);
+        len = ntohl(echo_rep_msg.LEN);
+        Rio_readn(clientfd,echo_rep_msg.BUFFER,sizeof(char)*len);
+        echo_rep_msg.BUFFER[len] = '\0';
+
+        pthread_mutex_lock(&mutex);
+        bprintf(fp_res,"[echo_rep](%d) %s\n",TID,echo_rep_msg.BUFFER);
+        pthread_mutex_unlock(&mutex);
+    }
+    Close(clientfd);
+    pthread_mutex_lock(&mutex);
+    bprintf(fp_res,"[cli](%d) connfd is closed!\n",TID);
+    bprintf(fp_res,"[cli](%d) parent thread is going to exit!\n",TID);
+    pthread_mutex_unlock(&mutex);
+}
+
+void *Write(void *vargp)
+{
+
+    Pthread_detach(pthread_self());
+    pthread_mutex_lock(&mutex);
+    bprintf(fp_res,"[cli](%d) pthread_detach is done!\n",TID);
+    pthread_mutex_unlock(&mutex);
+
+    char td_name[50];
+    sprintf(td_name,"td%d.txt",pin);
+    FILE *fp_read = Fopen(td_name,"r");
+    pdu echo_rqt_msg;
+    char rbuf[MAX_CMD_STR+1];
+
+    while (fgets(rbuf, sizeof(rbuf),fp_read)!=NULL) {
+        if(strncmp(rbuf,"exit",4) == 0) {
+            shutdown(clientfd,SHUT_WR);
+            pthread_mutex_lock(&mutex);
+            bprintf(fp_res,"[cli](%d) shutdown is called with SHUT_WR!\n",TID);
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        int len = strnlen(rbuf,MAX_CMD_STR);
+        rbuf[len] = '\0';
+        //建立echo_rqt_msg并发送
+        echo_rqt_msg.PIN = htonl(pin);
+        echo_rqt_msg.LEN = htonl(len);
+        strcpy(echo_rqt_msg.BUFFER,rbuf);
+        Rio_writen(clientfd,&echo_rqt_msg,sizeof(int)+sizeof(int)+len);
+    }
+    Fclose(fp_read);
+    return NULL;
+}
+
+void echo_rqt()
+{
+    pthread_t tid;
+    Pthread_create(&tid,NULL,Write,NULL);
+    Read();
+}
+
+int main(int argc,char **argv){
+    if(argc != 4){
+        fprintf(stderr,"usage: %s <host> <ip> <pin>",argv[0]);
+        exit(0);
+    }
+    pin = atoi(argv[3]);
+    TID = syscall(SYS_gettid);
+
+    char filename[MAXLINE];
+    sprintf(filename,"stu_cli_res_%d.txt",pin);
+    fp_res = Fopen(filename,"w");
+    bprintf(NULL,"[cli](%d) %s is created!\n",TID,filename);
+
+    clientfd = Open_clientfd(argv[1],argv[2]);
+    bprintf(fp_res,"[cli](%d) server[%s:%s] is connected!\n",TID,argv[1],argv[2]);
+
+    pthread_mutex_init(&mutex,NULL);
+    echo_rqt();
+
+    bprintf(NULL,"[cli](%d) %s is closed!\n",TID,filename);
+    exit(0);
+}
--- echo_mutilThread/tcp_echo_srv.c
+++ echo_mutilThread/tcp_echo_srv.c
@@ -5,175 +5,340 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <signal.h>
+#include <errno.h>
 #include <syscall.h>
 #include <pthread.h>
+#include <signal.h>
 
 #define MAX_CMD_STR 124
+#define MAXLINE 8192
+#define LISTENQ 8192
+
+#define bprintf(fp,format,...)\
+        if(fp==NULL){printf(format,##__VA_ARGS__);}\
+        else {printf(format,##__VA_ARGS__);\
+                fprintf(fp,format,##__VA_ARGS__);fflush(fp);}
 
 typedef struct sockaddr *pSA ;
-int sig_to_exit = 0;
+typedef void handler_t(int);
+
+FILE *stu_srv_res_p;
+pthread_mutex_t mutex;
+int TID;
 int sig_type = 0;
-FILE *fp_res;
-long tid;
-int pin;
+int sig_to_exit = 0;
 
 typedef struct PDU{
     int PIN;
     int LEN;
-    char BUF[MAX_CMD_STR+1];
+    char BUFFER[MAX_CMD_STR];
 }pdu;
 
-void sig_int(int signo) {
+void unix_error(char *msg) /* Unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+
+void posix_error(int code, char *msg) /* Posix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(code));
+    exit(0);
+}
+
+void sig_int(int signo)
+{
+    bprintf(stu_srv_res_p,"[srv](%d) SIGINT is coming!\n",TID);
     sig_type = signo;
     if(signo == SIGINT)
         sig_to_exit = 1;
 }
 
-void sig_pipe(int signo) {
-    sig_type = signo;
-    printf("[srv](%ld) SIGPIPE is coming!\n",tid);
-}
-
-void *echo_rep(void *arg) {
-
-    int sockfd = *(int*)arg;
-    char res_filename[MAX_CMD_STR];
-    memset(res_filename,0,sizeof(res_filename));
-    sprintf(res_filename,"stu_srv_res_%ld.txt",tid);
-    FILE *fn_res = fopen(res_filename,"wb");
-    if(fn_res == NULL){
-        printf("fn_res open failed!\n");
-        exit(1);
-    }
-    printf("[srv](%ld) %s is opened!\n",tid,res_filename);
-
-    char buffer[MAX_CMD_STR];
-    pdu echo_rev_pdu;
-    if(read(sockfd,&echo_rev_pdu,sizeof(pdu))==-1){
-        printf("[srv] echo_rep read error\n");
-        exit(1);
-    }
-
-    memset(buffer,0,sizeof(buffer));
-    sprintf(buffer,"[echo_rqt](%ld) %s",tid,echo_rev_pdu.BUF);
-    pin = echo_rev_pdu.PIN;
-    fputs(buffer,fn_res);
-
-    if(write(sockfd,&echo_rev_pdu,sizeof(pdu))==-1){
-        printf("[srv] echo_rep write error\n");
-        exit(1);
-    }
-
-    memset(buffer,0,sizeof(buffer));
-    sprintf(buffer,"[srv](%ld) connfd is closed!",tid);
-    fputs(buffer,fn_res);
-    close(sockfd);
-
-    printf("[srv](%ld) %s is closed!\n",tid,res_filename);
-    fclose(fn_res);
-
-    char res_filename_new[20];
-    memset(res_filename_new,0,sizeof(res_filename_new));
-    sprintf(res_filename_new,"stu_srv_res_%d.txt",pin);
-    if(rename(res_filename,res_filename_new) == 0){
-        printf("[srv](%ld) res file rename done!",tid);
-    }
-    else{
-        printf("rename error\n");
-        exit(1);
-    }
-
-}
-
-int main(int argc,char *argv[])
+void sig_pipe(int signo)
 {
-    char cli_ip[32];
-    struct sockaddr_in srv_addr,cli_addr;
-    socklen_t cli_addr_len;
-    int listenfd,connfd;
-    char buf[MAX_CMD_STR];
-    char file_name[MAX_CMD_STR];
+    bprintf(stu_srv_res_p,"[srv](%d) SIGPIPE is coming!\n",TID);
+    sig_type = signo;
+}
 
-    struct sigaction sigact_pipe, old_sigact_pipe;
-    sigact_pipe.sa_handler = sig_pipe;
-    sigemptyset(&sigact_pipe.sa_mask);
-    sigact_pipe.sa_flags = 0;
-    sigact_pipe.sa_flags |= SA_RESTART;
-    sigaction(SIGPIPE, &sigact_pipe, &old_sigact_pipe);
+handler_t *Signal(int signum, handler_t *handler)
+{
+    struct sigaction action, old_action;
 
-    struct sigaction sigact_int,old_sigact_int;
-    sigact_int.sa_handler = sig_int;
-    sigemptyset(&sigact_int.sa_mask);
-    sigact_int.sa_flags = 0;
-    sigact_pipe.sa_flags |= SA_RESTART;
-    sigaction(SIGINT,&sigact_int,&old_sigact_int);
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    if(signum != SIGINT)
+        action.sa_flags |= SA_RESTART;
 
+    if (sigaction(signum, &action, &old_action) < 0)
+        unix_error("Signal error");
+    return (old_action.sa_handler);
+}
 
-    if(argc != 3){
-        printf("Usage: server <IPaddress><Port>");
-        exit(1);
-    }
-    tid = syscall(SYS_gettid);
-    sprintf(file_name,"%s","stu_srv_res_p.txt");
-    printf("[srv](%ld) stu_srv_res_p.txt is opened!\n",tid);
-    fp_res = fopen(file_name,"wb");
-    if(fp_res==NULL){
-        printf("%s open failed!",file_name);
-        exit(1);
-    }
+void Close(int fd)
+{
+    if(close(fd)<0)
+        unix_error("Close error");
+}
+
+void Fclose(FILE *fp)
+{
+    if(fclose(fp)!=0)
+        unix_error("Fclose error");
+}
+
+int Socket(int domain, int type, int protocol)
+{
+    int rc;
+
+    if ((rc = socket(domain, type, protocol)) < 0)
+        unix_error("Socket error");
+    return rc;
+}
+
+void Bind(int sockfd, struct sockaddr *my_addr, int addrlen)
+{
+    int rc;
+
+    if ((rc = bind(sockfd, my_addr, addrlen)) < 0)
+        unix_error("Bind error");
+}
+
+void Listen(int s, int backlog)
+{
+    int rc;
+
+    if ((rc = listen(s,  backlog)) < 0)
+        unix_error("Listen error");
+}
+
+int Accept(int s, struct sockaddr *addr, socklen_t *addrlen)
+{
+    int rc;
+
+    if ((rc = accept(s, addr, addrlen)) < 0)
+        unix_error("Accept error");
+    return rc;
+}
+
+int open_listenfd(char *ip,char *port)
+{
+    int listenfd,optval=1;
+    struct sockaddr_in srv_addr;
+
+    listenfd = Socket(AF_INET,SOCK_STREAM,0);
 
     bzero(&srv_addr,sizeof(srv_addr));
     srv_addr.sin_family = AF_INET;
-    inet_pton(AF_INET,argv[1],&srv_addr.sin_addr);
-    srv_addr.sin_port = htons(atoi(argv[2]));
-
-    listenfd = socket(AF_INET,SOCK_STREAM,0);
-    if( listenfd== -1){
-        printf("[srv] socket error\n");
-        exit(1);
-    }
-
+    inet_pton(AF_INET,ip,&srv_addr.sin_addr);
+    srv_addr.sin_port = htons(atoi(port));
     // 在bind之前打印输出监听情况
-    char srv_ip[32];
-    inet_ntop(AF_INET, &srv_addr.sin_addr,srv_ip, sizeof(srv_ip));
-    sprintf(buf,"[srv](%ld) server[%s:%hu] is initializing!\n",tid,srv_ip,ntohs(srv_addr.sin_port));
-    fputs(buf,fp_res);
+    bprintf(stu_srv_res_p,"[srv](%d) server[%s:%s] is initializing!\n",TID,ip,port);
+    Bind(listenfd,(pSA)&srv_addr,sizeof(struct sockaddr_in));
 
-    if(bind(listenfd,(pSA)&srv_addr,sizeof(struct sockaddr_in)) == -1){
-        printf("[srv] bind error\n");
-        exit(1);
+    Listen(listenfd,LISTENQ);
+    return listenfd;
+}
+
+int Open_listenfd(char *ip,char *port)
+{
+    int rc;
+    if((rc = open_listenfd(ip,port))<0)
+        unix_error("Open_listenfd");
+    return rc;
+}
+
+void Rename(char *oldname, char *newname)
+{
+    if(rename(oldname,newname)<0)
+        unix_error("Rename error");
+}
+
+FILE *Fopen(const char *filename,const char *mode)
+{
+    FILE *fp;
+    if((fp = fopen(filename,mode))==NULL)
+        unix_error("Fopen error");
+    return fp;
+}
+
+ssize_t rio_readn(int fd, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nread = read(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) /* Interrupted by sig handler return */
+                nread = 0;      /* and call read() again */
+            else
+                return -1;      /* errno set by read() */
+        }
+        else if (nread == 0)
+            break;              /* EOF */
+        nleft -= nread;
+        bufp += nread;
+    }
+    return (n - nleft);         /* return >= 0 */
+}
+
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR)  /* Interrupted by sig handler return */
+                nwritten = 0;    /* and call write() again */
+            else
+                return -1;       /* errno set by write() */
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
+    }
+    return n;
+}
+
+
+void Rio_writen(int fd,void *userbuf,size_t n)
+{
+    if(rio_writen(fd,userbuf,n) != n)
+        unix_error("Rio_writen error");
+}
+
+void Rio_readn(int fd,void *userbuf,size_t n)
+{
+    if(rio_readn(fd,userbuf,n)<0)
+        unix_error("Rio_read error");
+
+}
+
+void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp,
+                    void * (*routine)(void *), void *argp)
+{
+    int rc;
+
+    if ((rc = pthread_create(tidp, attrp, routine, argp)) != 0)
+        posix_error(rc, "Pthread_create error");
+}
+
+void Pthread_cancel(pthread_t tid) {
+    int rc;
+
+    if ((rc = pthread_cancel(tid)) != 0)
+        posix_error(rc, "Pthread_cancel error");
+}
+
+void Pthread_join(pthread_t tid, void **thread_return) {
+    int rc;
+
+    if ((rc = pthread_join(tid, thread_return)) != 0)
+        posix_error(rc, "Pthread_join error");
+}
+
+void Pthread_detach(pthread_t tid) {
+    int rc;
+
+    if ((rc = pthread_detach(tid)) != 0)
+        posix_error(rc, "Pthread_detach error");
+}
+
+void Pthread_exit(void *retval) {
+    pthread_exit(retval);
+}
+
+pthread_t Pthread_self(void) {
+    return pthread_self();
+}
+
+void Pthread_once(pthread_once_t *once_control, void (*init_function)()) {
+    pthread_once(once_control, init_function);
+}
+
+void echo(int sockfd)
+{
+    int pin;
+    int len;
+    pdu echo_rqt_msg,echo_rep_msg;
+    char fn_res[50];
+    sprintf(fn_res,"stu_srv_res_%d.txt",TID);
+    FILE *fp_res = Fopen(fn_res,"w");
+    bprintf(NULL,"[srv](%d) %s is opened!\n",TID,fn_res);
+
+    memset(&echo_rqt_msg,0,sizeof(echo_rqt_msg));
+    while (rio_readn(sockfd,&echo_rqt_msg.PIN,sizeof(int))>0){
+        memset(&echo_rep_msg,0,sizeof(echo_rep_msg));
+
+        Rio_readn(sockfd,&echo_rqt_msg.LEN,sizeof(int));
+        pin = ntohl(echo_rqt_msg.PIN);
+        len = ntohl(echo_rqt_msg.LEN);
+        Rio_readn(sockfd,echo_rqt_msg.BUFFER,sizeof(char)*len);
+        echo_rqt_msg.BUFFER[len] = '\0';
+        bprintf(fp_res,"[echo_rqt](%d) %s\n",TID,echo_rqt_msg.BUFFER);
+
+        //echo_rep_msg发送
+        echo_rep_msg = echo_rqt_msg;
+        Rio_writen(sockfd,&echo_rep_msg,sizeof(int)+sizeof(int)+len);
+        memset(&echo_rqt_msg,0,sizeof(echo_rqt_msg));
     }
 
-    if(listen(listenfd,100) == -1){
-        printf("[srv] listen error\n");
-        exit(1);
+    Close(sockfd);
+    bprintf(fp_res,"[srv](%d) connfd is closed!\n",TID);
+    Fclose(fp_res);
+    bprintf(NULL,"[srv](%d) %s is closed!\n",TID,fn_res);
+
+    char fn_res_new[50];
+    memset(fn_res_new,0,sizeof(fn_res_new));
+    sprintf(fn_res_new,"stu_srv_res_%d.txt",pin);
+    Rename(fn_res, fn_res_new);
+    bprintf(NULL,"[srv](%d) res file rename done!\n",TID);
+}
+
+void *thread(void *vargp)
+{
+    int fd = *((int*)vargp);
+    Pthread_detach(Pthread_self());
+    echo(fd);
+    return NULL;
+}
+
+int main(int argc,char **argv)
+{
+    TID = syscall(SYS_gettid);
+    int listenfd,connfd;
+    socklen_t clientlen;
+    struct sockaddr_in  clientaddr;
+    pthread_t tid;
+
+    if(argc !=3){
+        fprintf(stderr,"usage: %s <host> <port>",argv[0]);
+        exit(0);
     }
+    stu_srv_res_p = Fopen("stu_srv_res_p.txt","w");
+    bprintf(NULL,"[srv](%d) stu_srv_res_p.txt is opened!\n",TID);
 
+    Signal(SIGINT,sig_int);
+    Signal(SIGPIPE,sig_pipe);
+    listenfd = Open_listenfd(argv[1],argv[2]);
 
-    while(!sig_to_exit)
-    {
-        cli_addr_len = sizeof(cli_addr);
-        connfd = accept(listenfd,(pSA)&cli_addr,&cli_addr_len);
-        if (connfd < 0) continue;
+    char client_ip[32];
+    while(!sig_to_exit){
+        clientlen = sizeof(struct sockaddr_in);
+        connfd = accept(listenfd,(pSA)&clientaddr,&clientlen);
+        if(connfd <0 ) continue;
 
-        pthread_t socket_rep;
-        pthread_create(&socket_rep,NULL,echo_rep,&connfd);
-
-        memset(cli_ip,0,sizeof(cli_ip));
-        memset(buf,0,sizeof(buf));
-        inet_ntop(AF_INET, &cli_addr.sin_addr,cli_ip, sizeof(cli_ip));
-        sprintf(buf,"[srv](%ld) client[%s:%hu] is accepted!",tid,cli_ip,ntohs(cli_addr.sin_port));
-        fputs(buf,fp_res);
-        pthread_join(socket_rep,NULL);
+        memset(client_ip,0,sizeof(client_ip));
+        inet_ntop(AF_INET, &clientaddr.sin_addr,client_ip, sizeof(client_ip));
+        bprintf(stu_srv_res_p,"[srv](%d) client[%s:%hu] is accepted!\n",TID,client_ip,ntohs(clientaddr.sin_port));
+        Pthread_create(&tid,NULL,thread,&connfd);
     }
-
-    close(listenfd);
-    memset(buf,0,sizeof(buf));
-    sprintf(buf,"[srv](%ld) listenfd is closed!",tid);
-    fputs(buf,fp_res);
-
-    fclose(fp_res);
-    printf("[srv])(%ld) %s is closed!\n",tid,file_name);
+    Close(listenfd);
+    bprintf(stu_srv_res_p,"[srv](%d) listenfd is closed!\n",TID);
+    Fclose(stu_srv_res_p);
+    bprintf(NULL,"[srv](%d) stu_srv_res_p.txt is closed!\n",TID);
     return 0;
 }
